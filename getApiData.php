e8adf91ab56a35c83872e801c840ce2c4614c2ba
Gerard <gerard.r.hanley@gmail.com>
2021-02-21 11:41:30 +0000

    k

--- getApiData.php
+++ getApiData.php
@@ -3,9 +3,6 @@
 
 
 $errorEmpty = false;
-
-
-
 $executionStartTime = microtime(true) / 1000;
 
 $geoBeginning = 'http://api.geonames.org/';
@@ -16,51 +13,54 @@ $identifier2 = $_REQUEST['ident2'];
 $value2 = $_REQUEST['val2'];
 $username = '&username=gh2021';
 
+// check for empty fields in form that has been clicked.
+if(empty($api) ||empty($identifier1)||empty($value1)||empty($identifier2)||empty($value2)){
 
-if(empty($api) ||empty($identifier1)||empty($value1)){
-
-    echo '<span class="results-message">Fill in all fields</span>';
-    $errorEmpty = true;
-
-
-
-}
-
-
-
-
-
-$url=$geoBeginning.$api.$identifier1.$value1.$identifier2.$value2.$username;
-
-
-$select = $_REQUEST['select'];
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url);
-
-$result=curl_exec($ch);
-
-curl_close($ch);
-
-$decode = json_decode($result,true);	
-
-
-$output['status']['code'] = "200";
-$output['status']['name'] = "ok";
-$output['status']['description'] = "mission saved";
-$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-$output['status']['message'] = $decode;
-
-if($select == 'none'){
-    $output['data'] = $decode;
+    $output['status']['code'] = "400";
+    $output['status']['name'] = "error";
+    $output['status']['description'] = "mission failed";
+    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+    $output['status']['message'] = 'Field not filled in!';
+    $output['status']['error'] = true;
+    
 }else{
-    $output['data'] = $decode[$select];
+        $url=$geoBeginning.$api.$identifier1.$value1.$identifier2.$value2.$username;
+
+
+        $select = $_REQUEST['select'];
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL,$url);
+
+        $result=curl_exec($ch);
+
+        curl_close($ch);
+
+        $decode = json_decode($result,true);	
+
+        $output['status']['code'] = "200";
+        $output['status']['name'] = "ok";
+        $output['status']['description'] = "mission saved";
+        $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+        $output['status']['message'] = $decode;
+        $output['status']['error'] = false;
+
+
 }
+    
 
-header('Content-Type: application/json; charset=UTF-8');
+    
+    
+    if($select == 'none'){
+        $output['data'] = $decode;
+    }else{
+        $output['data'] = $decode[$select];
+    }
 
-echo json_encode($output); 
+    header('Content-Type: application/json; charset=UTF-8');
+
+    echo json_encode($output); 
 
 ?>
